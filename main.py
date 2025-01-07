30f3212d0972ed3074409cbb8583e8562c99cf93
Arkin9 <90937361+Arkin9@users.noreply.github.com>
2025-01-07 12:51:12 +0530

    Removed unecessary imports

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
@@ -146,3 +143,4 @@ if __name__ == "__main__":
 
     # Quit the driver
     driver.quit()
+
